39bfb1e459281832f8715441e071fe35cf702fa7
xingwei <xingwei@soundai.com>
2019-09-09 17:15:19 +0800

    finish

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,19 +4,29 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class Practice13GetTextBoundsView extends View {
     Paint paint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
     Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint paint3 = new Paint(Paint.ANTI_ALIAS_FLAG);
     String text1 = "A";
     String text2 = "a";
     String text3 = "J";
     String text4 = "j";
     String text5 = "Â";
     String text6 = "â";
+    Rect rect1 = new Rect();
+    Rect rect2 = new Rect();
+    Rect rect3 = new Rect();
+    Rect rect4 = new Rect();
+    Rect rect5 = new Rect();
+    Rect rect6 = new Rect();
+
     int top = 200;
     int bottom = 400;
 
@@ -37,6 +47,8 @@ public class Practice13GetTextBoundsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        paint3.setStyle(Paint.Style.STROKE);
     }
 
     @Override
@@ -49,12 +61,25 @@ public class Practice13GetTextBoundsView extends View {
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
+
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        paint2.getTextBounds(text1, 0, text1.length(), rect1);
+        canvas.drawText(text1, 100, middle + ((rect1.bottom - rect1.top) >> 1) - rect1.bottom, paint2);
+
+        paint2.getTextBounds(text2, 0, text2.length(), rect2);
+        canvas.drawText(text2, 200, middle + ((rect2.bottom - rect2.top) >> 1)- rect2.bottom, paint2);
+
+        paint2.getTextBounds(text3, 0, text3.length(), rect3);
+        canvas.drawText(text3, 300, middle + ((rect3.bottom - rect3.top) >> 1)- rect3.bottom, paint2);
+
+        paint2.getTextBounds(text4, 0, text4.length(), rect4);
+        canvas.drawText(text4, 400, middle + ((rect4.bottom - rect4.top) >> 1)- rect4.bottom, paint2);
+
+        paint2.getTextBounds(text5, 0, text5.length(), rect5);
+        canvas.drawText(text5, 500, middle + ((rect5.bottom - rect5.top) >> 1)- rect5.bottom, paint2);
+
+        paint2.getTextBounds(text6, 0, text6.length(), rect6);
+        canvas.drawText(text6, 600, middle + ((rect6.bottom - rect6.top) >> 1)- rect6.bottom, paint2);
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class Practice14GetFontMetricsView extends View {
@@ -14,6 +15,12 @@ public class Practice14GetFontMetricsView extends View {
     String[] texts = {"A", "a", "J", "j", "Â", "â"};
     int top = 200;
     int bottom = 400;
+    float fontMetrics_top;
+    float fontMetrics_ascent;
+    float fontMetrics_descent;
+    float fontMetrics_bottom;
+    float fontMetrics_leading;
+    float offset = 0;
 
     public Practice14GetFontMetricsView(Context context) {
         super(context);
@@ -32,6 +39,14 @@ public class Practice14GetFontMetricsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        fontMetrics_top = paint2.getFontMetrics().top;
+        fontMetrics_ascent = paint2.getFontMetrics().ascent;
+        fontMetrics_descent = paint2.getFontMetrics().descent;
+        fontMetrics_bottom = paint2.getFontMetrics().bottom;
+        fontMetrics_leading = paint2.getFontMetrics().leading;
+        offset = -(fontMetrics_descent + fontMetrics_ascent) / 2;
+        Log.d("metrics", "FontMetrics:" + paint2.getFontMetricsInt());
     }
 
     @Override
@@ -45,11 +60,11 @@ public class Practice14GetFontMetricsView extends View {
         // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(texts[0], 100, middle, paint2);
-        canvas.drawText(texts[1], 200, middle, paint2);
-        canvas.drawText(texts[2], 300, middle, paint2);
-        canvas.drawText(texts[3], 400, middle, paint2);
-        canvas.drawText(texts[4], 500, middle, paint2);
-        canvas.drawText(texts[5], 600, middle, paint2);
+        canvas.drawText(texts[0], 100, middle + offset, paint2);
+        canvas.drawText(texts[1], 200, middle + offset, paint2);
+        canvas.drawText(texts[2], 300, middle + offset, paint2);
+        canvas.drawText(texts[3], 400, middle + offset, paint2);
+        canvas.drawText(texts[4], 500, middle + offset, paint2);
+        canvas.drawText(texts[5], 600, middle + offset, paint2);
     }
 }
